6451e3a5f1047c5a5ca7222911673e93fae1f823
doubleshendi <78292834+doubleshendi@users.noreply.github.com>
2021-02-04 14:54:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// This is the function that does
+// everything in this example.
+function showMessage() {
+  // This hello message is definitely not nice.
+  //alert("Hello, I am the web wizard. I see that you have tampered with my code. Your feeble intelligence would make a flea feel proud by comparison.");
+
+  // But this hello messsage is nice.
+  alert("Hello, I am Princess fairy Suchita. Your potential is great. Soon you will join me in programming mastery.");
+}
